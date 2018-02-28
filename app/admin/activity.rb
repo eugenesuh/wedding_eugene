dc696c2d88655423fd6a70514bea5100b634c5da
Raghu Betina <raghu@firstdraft.com>
2018-02-28 03:11:11 +0000

    Whitelist columns in Activity for ActiveAdmin

--- app/admin/activity.rb
+++ app/admin/activity.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Activity do
 
+ permit_params :title, :description, :user_id, :image
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
