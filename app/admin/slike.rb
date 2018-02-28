8921f0d09b36524e478ccda9542bee96c40db6f1
Raghu Betina <raghu@firstdraft.com>
2018-02-28 03:11:18 +0000

    Whitelist columns in Slike for ActiveAdmin

--- app/admin/slike.rb
+++ app/admin/slike.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Slike do
 
+ permit_params :user_id, :song_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
