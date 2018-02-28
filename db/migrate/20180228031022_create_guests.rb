163c59f3a55aff34a675bb1955ab3a222e3959ff
Raghu Betina <raghu@firstdraft.com>
2018-02-28 03:10:22 +0000

    Generate guests resource with starter:resource

---
+++ db/migrate/20180228031022_create_guests.rb
@@ -0,0 +1,17 @@
+class CreateGuests < ActiveRecord::Migration
+  def change
+    create_table :guests do |t|
+      t.integer :user_id
+      t.integer :age
+      t.string :status
+      t.integer :food
+      t.string :lastname
+      t.string :firstname
+      t.string :dietary
+      t.string :image
+
+      t.timestamps
+
+    end
+  end
+end
