eabe5225cbfd800852e58e6285659f8858fac178
Raghu Betina <raghu@firstdraft.com>
2018-02-28 03:10:49 +0000

    Direct association between users and accomodations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :accomodations,
+             :dependent => :destroy
+
   has_many   :scomments,
              :dependent => :destroy
 
